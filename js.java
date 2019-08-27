284ef1cfa2b4eebecda75c12d0b09eb349c3efe5
993640191 <44936811+993640191@users.noreply.github.com>
2019-08-27 09:56:30 +0800

    Add files via upload

---
+++ js.java
@@ -0,0 +1,10 @@
+package com.csii.jsTest;
+
+public class js {
+    static String str="123";
+
+    public static void main(String[] args){
+        System.out.println(Integer.parseInt(str));
+
+    }
+}
